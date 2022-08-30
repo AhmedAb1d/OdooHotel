974728502a5673fa50aac6113ab49b49e0e971dd
Ahmed Abid <92954851+AhmedAb1d@users.noreply.github.com>
2022-08-30 20:23:52 +0100

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name' : 'Hotel Management',
+    'version' : '1.2',
+    'summary': 'This is a hotel management model',
+    'sequence': 10,
+    'description': """test""",
+    'category': 'Productivity',
+    'website': 'this is a website',
+    'depends' : [],
+    'data': [
+        'views/client.xml',
+        'security/ir.model.access.csv'
+    ],
+    'demo': [
+        
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'assets': {
+        
+    },
+    'license': 'LGPL-3',
+}
---
+++ models/client.py
@@ -0,0 +1,15 @@
+from odoo import api, fields, models
+
+
+class HotelClient(models.Model):
+    _name = "hotel.client"
+    _description = "Hotel client"
+
+    name = fields.Char(string='Name', required=True)
+    age=fields.Integer(string='Age')
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other'),
+    ], required=True, default='male')
+    note = fields.Text(string='Description')
